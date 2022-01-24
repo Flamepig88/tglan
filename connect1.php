1082ef393b828b0ca3193d78942a2f93c7159704
Rabi Zeito <rabi.88@hotmail.com>
2022-01-24 23:26:35 +0100

    init

---
+++ connect1.php
@@ -0,0 +1,19 @@
+<?php
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $bio = $_POST['bio'];
+
+    //Database Connection
+    $conn = new mysqli('localhost', 'root', '', 'boka');
+    if($conn->connect_error){
+        die('Connection Failed  : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(username, email, bio)
+        values(?, ?, ?)");
+        $stmt->bind_param("sss",$username, $email, $bio);
+        $stmt->execute();
+        echo "Din förfrågan har skickats in! Du kan stänga ner detta fönster.";
+        $stmt->close();
+        $conn->close();
+    }
+?>
