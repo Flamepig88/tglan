d26bbaa3bb8e727c821569851e4717b260065b3a
Flamepig88 <rabi.88@hotmail.com>
2022-01-27 21:08:19 +0100

    init

--- javascript/uppgifter.js
+++ javascript/uppgifter.js
@@ -1,4 +1,4 @@
-//1. 
+//1.
 
 //const apa = "christian"
 //const age = 12
@@ -6,40 +6,40 @@
 //const greeting = "Hej " + apa + " du är " + age + " år gammal"
 
 const results = [
-    { apa: 'Micke', score: 76 },
-    { apa: 'Leo', score: 65 },
-    { apa: 'Danne', score: 34 },
-    { apa: 'Madde', score: 77 },
-    { apa: 'Mehmet', score: 87 },
-    { apa: 'Anna', score: 76 },
-    { apa: 'Markus', score: 54 },
-    { apa: 'Samuel', score: 63 },
-    { apa: 'Helene', score: 58 },
-    { apa: 'Nina', score: 82 },
-   ]
-    
-       //array.forEach(function(element) {
-              //})
+  { name: "Micke", score: 76 },
+  { name: "Leo", score: 65 },
+  { name: "Danne", score: 34 },
+  { name: "Madde", score: 77 },
+  { name: "Mehmet", score: 87 },
+  { name: "Anna", score: 76 },
+  { name: "Markus", score: 54 },
+  { name: "Samuel", score: 63 },
+  { name: "Helene", score: 58 },
+  { name: "Nina", score: 82 },
+];
+
+function getHighestScore()
+
+//array.forEach(function(element) {
+//})
 
 //for (let i = 0; i < results.length; i++) {
-  //  console.log(results[i])
+//  console.log(results[i])
 //}
 //const testScore = `Hej ${apa} du fick ${score} på provet`
 
-results.forEach(function (result) {
-    console.log(result.name, 'fick', result.score, 'poäng')
-  })
+//for (let i = 0; i < results.length; i++) {
+//console.log(`${results[i].name} fick ${results[i].score} poäng på provet`);  // -----------funkar
+//}
 
-  const numbers = [5, 6, 2, 6]
+//const numbers = [5, 6, 2, 6];
 
-  let sum = 0
-  for (let number of numbers) {
-      sum = sum + number
-  }
-  comsole.log("Sum: ", sum)
+//let sum = 0;
+//for (let number of numbers) {
+//  sum = sum + number;
+//}
+//comsole.log("Sum: ", sum);
 
-  let totalIncome = 0
+//let totalIncome = 0;
 
-  account.incomes.forEach(income => {
-      
-  })
+//account.incomes.forEach((income) => {});
