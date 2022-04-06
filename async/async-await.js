423c042b202e044c06408d9a8bbd7729d88c3da9
Flamepig88 <rabi.88@hotmail.com>
2022-04-06 22:05:00 +0200

    väder app

--- async/async-await.js
+++ async/async-await.js
@@ -20,27 +20,87 @@
 /* ------- Exempel 2---------- */
 
 function printWeather() {
-    document.querySelector('#weather-container').innerHTML = ''
-    fetch('http://api.weatherapi.com/v1/current.json?key=f74998a310f641339ec123218220104%20&q=Sweden,%20Gothenburg&aqi=no')
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        const iconUrl = data.icon_url
-        const joke = data.value
-  
-        const weatherContainer = document.querySelector('#weather-container')
-  
-        const imgEl = document.createElement('img')
-        imgEl.setAttribute('src', iconUrl)
-  
-        const textEl = document.createElement('h2')
-        textEl.textContent = weather
-  
-        weatherContainer.appendChild(imgEl)
-        weatherContainer.appendChild(textEl)
-      })
-      .catch(err => console.log(err))
-  }
-  
-  document.querySelector('#btn').addEventListener('click', printWeather)
+  document.querySelector("#weather-container").innerHTML = "";
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=f74998a310f641339ec123218220104%20&q=Sweden,%20Gothenburg&aqi=no"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const location = data.location.country;
+      const iconUrl = data.current.condition.icon;
+      const weather = data.current.temp_c;
+      const time = data.location.localtime;
+      const weatherCondition = data.current.condition.text;
+
+      const weatherContainer = document.querySelector("#weather-container");
+
+      const imgEl = document.createElement("img");
+      imgEl.setAttribute("src", iconUrl);
+
+      const textEl = document.createElement("h2");
+      textEl.textContent = weather;
+
+      const textEl2 = document.createElement("h2");
+      textEl2.textContent = location;
+
+      const textEl3 = document.createElement("h2");
+      textEl3.textContent = time;
+
+      const textEl4 = document.createElement("h2");
+      textEl4.textContent = weatherCondition;
+
+      weatherContainer.appendChild(imgEl);
+      weatherContainer.appendChild(textEl);
+      weatherContainer.appendChild(textEl2);
+      weatherContainer.appendChild(textEl3);
+      weatherContainer.appendChild(textEl4);
+    })
+    .catch((err) => console.log(err));
+}
+
+document.querySelector("#btn").addEventListener("click", printWeather);
+
+function printWeatherSpain() {
+  document.querySelector("#weather-container1").innerHTML = "";
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=f74998a310f641339ec123218220104&q=Spain&aqi=no"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const location1 = data.location.country;
+      const iconUrl1 = data.current.condition.icon;
+      const weather1 = data.current.temp_c;
+      const time = data.location.localtime;
+      const weatherCondition = data.current.condition.text;
+
+      const weatherContainer = document.querySelector("#weather-container1");
+
+      const imgEl = document.createElement("img");
+      imgEl.setAttribute("src", iconUrl1);
+
+      const textEl = document.createElement("h2");
+      textEl.textContent = weather1;
+
+      const textEl2 = document.createElement("h2");
+      textEl2.textContent = location1;
+
+      const textEl3 = document.createElement("h2");
+      textEl3.textContent = time;
+
+      const textEl4 = document.createElement("h2");
+      textEl4.textContent = weatherCondition;
+
+      weatherContainer.appendChild(imgEl);
+      weatherContainer.appendChild(textEl);
+      weatherContainer.appendChild(textEl2);
+      weatherContainer.appendChild(textEl3);
+      weatherContainer.appendChild(textEl4);
+    })
+    .catch((err) => console.log(err));
+}
+
+document.querySelector("#btn1").addEventListener("click", printWeatherSpain);
