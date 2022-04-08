be21e484579c8e5942a6c22bb66a41a569afb501
Flamepig88 <rabi.88@hotmail.com>
2022-04-08 14:17:05 +0200

    väder saker

--- async/async-await.js
+++ async/async-await.js
@@ -13,22 +13,24 @@ button.addEventListener('click', function () {
 
     const weatherContainer = document.querySelector(".weather-container");
 
-    const countryName = data.location.country;
+    // const countryName = data.location.country;
     const cityName = data.location.name;
     const iconUrl = data.current.condition.icon;
     const weather = data.current.temp_c;
     const time = data.location.localtime;
     const weatherCondition = data.current.condition.text;
     const feelsLike = data.current.feelslike_c;
+    const humidity = data.current.humidity;
+    const uv = data.current.uv;
 
     const imgEl = document.createElement("img");
     imgEl.setAttribute("src", iconUrl);
 
     const textEl = document.createElement("h2");
-    textEl.textContent = weather;
+    textEl.textContent = weather + ' °C / ' + 'Känns som ' + feelsLike + ' °C';
 
-    const textEl2 = document.createElement("h2");
-    textEl2.textContent = countryName;
+    // const textEl2 = document.createElement("h2");
+    // textEl2.textContent = countryName;
 
     const textEl6 = document.createElement("h2");
     textEl6.textContent = cityName;
@@ -39,16 +41,25 @@ button.addEventListener('click', function () {
     const textEl4 = document.createElement("h2");
     textEl4.textContent = weatherCondition;
 
-    const textEl5 = document.createElement("h2");
-    textEl5.textContent = feelsLike;
+    // const textEl5 = document.createElement("h2");
+    // textEl5.textContent = 'Känns som ' + feelsLike + ' °C';
+
+    const textEl7 = document.createElement("h2");
+    textEl7.textContent = 'Luftfuktighet: ' + humidity + '%';
+    
+    const textEl8 = document.createElement("h2");
+    textEl8.textContent = 'UV-strålning: ' + uv;
+
 
     weatherContainer.appendChild(imgEl);
     weatherContainer.appendChild(textEl);
-    weatherContainer.appendChild(textEl5);
-    weatherContainer.appendChild(textEl2);
+    // weatherContainer.appendChild(textEl5);
+    // weatherContainer.appendChild(textEl2);
     weatherContainer.appendChild(textEl6);
     weatherContainer.appendChild(textEl3);
     weatherContainer.appendChild(textEl4);
+    weatherContainer.appendChild(textEl7);
+    weatherContainer.appendChild(textEl8);
     
 })
 document.querySelector(".weather-container").innerHTML = "";
